6bea78d340602076b8bbbf2dac9efcf074503372
shorya1104 <93487708+shorya1104@users.noreply.github.com>
2023-09-16 23:35:55 +0530

    changes in script

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 #Pull the docker image
-docker pull shoryasngh/env:latest
+sudo docker pull shoryasngh/env:latest
 
 #run docker container 
-docker container run -d -p 3000:3000 -name ENV env:latest
+sudo docker container run -d -p 3000:3000 -name ENV env:latest
