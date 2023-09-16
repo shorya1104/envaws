ab55e009e09539328b548bde88f9681ab255c14d
shorya1104 <93487708+shorya1104@users.noreply.github.com>
2023-09-17 01:42:04 +0530

    changes

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-containerid=`sudo docker ps -a | awk 'NR==2 {print $1}'`
-sudo docker container stop $containerid
-sudo docker container remove $containerid
+CONTAINERID="sudo docker ps -a | awk 'NR==2 {print $1}'"
+sudo docker container stop $CONTAINERID
+sudo docker container remove $CONTAINERID
